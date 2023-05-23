ea7b6fdf94ef3f8ea35a1559294001396330a9c6
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2023-05-23 15:47:40 +0200

    Done cleaning and commenting

--- warehouse/src/include/employee.hpp
+++ warehouse/src/include/employee.hpp
@@ -9,10 +9,48 @@ class Employee{
         bool busy = false;
         bool forkliftCertificate;
     public:
+        /**
+         * @brief Constructor
+         * 
+         * @param name 
+         * @param forkliftCertificate 
+         */
         Employee(std::string name, bool forkliftCertificate);
+
+        /**
+         * @brief Get the Name of an employee
+         * 
+         * @return std::string 
+         */
         std::string getName();
+
+        /**
+         * @brief Check if an employee is busy
+         * 
+         * @return true when busy
+         * @return false when not busy
+         */
         bool getBusy();
+
+        /**
+         * @brief Change if an employee is busy
+         * 
+         * @param busy true if busy, false if not busy
+         */
         void setBusy(bool busy);
+
+        /**
+         * @brief Check if an employee is forkliftcertified
+         * 
+         * @return true
+         * @return false 
+         */
         bool getForkliftCertificate();
+
+        /**
+         * @brief Set if an employee is forkliftcertified
+         * 
+         * @param forkliftCertificate true when forkliftcertified, false when not so
+         */
         void setForkliftCertificate(bool forkliftCertificate);
 };
--- warehouse/src/include/pallet.hpp
+++ warehouse/src/include/pallet.hpp
@@ -27,18 +27,96 @@ class Pallet: public IContainer{
         Pallet();
 
         /**
-         * @brief Get the Item Name object
+         * @brief Get the name of items on a pallet
          * 
-         * @return std::string 
+         * @return std::string the name
          */
         std::string getItemName();
+
+        /**
+         * @brief Get the amount of items on a pallet
+         * 
+         * @return int the amount
+         */
         int getItemCount();
+
+        /**
+         * @brief Get the amount of items that can still be added to the pallet
+         * 
+         * @return int (itemCapacity - itemCount)
+         */
         int getRemainingSpace();
+
+        /**
+         * @brief Allocate an empty pallet to a new item type
+         * 
+         * @param itemName the new item type
+         * @param itemCapacity the new capacity
+         * 
+         * @details Only succeeds when the pallet is empty
+         * 
+         * @return true when succeeded
+         * @return false when unable to succeed
+         */
         bool reallocateEmptyPallet(std::string itemName, int itemCapacity);
+
+        /**
+         * @brief Set the Item Capacity for a pallet
+         * 
+         * @param itemCapacity the new item capacity
+         * 
+         * @details Only succeeds when the old item capacity was unset (-1)
+         * 
+         * @return true when succeeded
+         * @return false when unable to succeed
+         */
         bool setItemCapacity(int itemCapacity);
+
+        /**
+         * @brief Set the Item Name for a pallet
+         * 
+         * @param itemName the new item name
+         * 
+         * @details Only succeeds when the old item name was unset/empty ("")
+         * 
+         * @return true when succeeded
+         * @return false when unable to succeed
+         */
         bool setItemName(std::string itemName);
+
+        /**
+         * @brief Reduce the itemCount by 1
+         * 
+         * @details Only succeeds when the item count was not 0
+         * 
+         * @return true when succeeded
+         * @return false when unable to succeed
+         */
         bool takeOne();
+
+        /**
+         * @brief Add 1 to the itemCount
+         * 
+         * @details Only succeeds when the item count was not equal to the item capacity
+         * 
+         * @return true when succeeded
+         * @return false when unable to succeed
+         */
         bool putOne();
+
+        /**
+         * @brief Check if itemCount is 0
+         * 
+         * @return true
+         * @return false 
+         */
         bool isEmpty() override;
+
+        /**
+         * @brief Check if itemCount is equal to itemCapacity
+         * 
+         * @return true 
+         * @return false 
+         */
         bool isFull() override;
 };
